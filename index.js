c8a90ba9f1840b25ba726db2597fbe4524d35c4e
kimhui9027 <66947451+kimhui9027@users.noreply.github.com>
2020-06-16 15:43:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -103,6 +103,30 @@ client.on('message', (message) => {
     message.reply('뒤질래?');
   }
 
+  if(message.content.startsWith('!전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
+  }
+
 });
+
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
 
 client.login(token);
