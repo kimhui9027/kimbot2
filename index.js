58d37dfea507c993dc2a78d4c09eab625edd3a54
kimhui9027 <66947451+kimhui9027@users.noreply.github.com>
2020-07-01 13:03:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -288,11 +288,6 @@ client.on('message', (message) => {
     message.channel.send(embed)
   }
 
-    embed.addField('Commands: ', commandStr);
-
-    message.channel.send(embed)
-  }
-
   if(message.content == '후이야 도움말 공지') {
     let img = '';
     let embed = new Discord.RichEmbed()
